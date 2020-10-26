893adf97449abb3adb4318d483ab13994a82517d
kdevanath <kiran.devanath@gmail.com>
2020-10-26 05:38:04 -0400

    Proper directory structure

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "f0ae4ebbe90fbd9e75aba9d0d2e6f2b5"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAWAU-tvOCEGxg2mOaL09fngW_vIb3ozsU"
